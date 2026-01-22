fea5d809b1137da03b076d9d1ee9793836c22353
CyberKyoujin <cyber.kyoujin@gmail.com>
2026-01-22 23:59:43 +0100

    fix

--- frontend-admin/frontend-admin/playwright.config.ts
+++ frontend-admin/frontend-admin/playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://127.0.0.1:5000',
+    baseURL: 'http://localhost:5000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -64,8 +64,8 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: 'npx vite --port 5000',
-    url: 'http://127.0.0.1:5000',
+    command: 'npx vite --port 5000 --host',
+    url: 'http://localhost:5000',
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
     stdout: 'pipe',
